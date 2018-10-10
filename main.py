d658c56a881ccb1350a0f4468e416fac43596d2b
Dylan <dambrad2@southernct.edu>
2018-10-10 16:24:57 -0400

    lots of changes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import textwrap
+
 import httplib2
 import numpy as np
 from googleapiclient.discovery import build
@@ -25,12 +27,11 @@ def authorize_credentials():
     return credentials
 
 
-def get_google_sheet(spreadsheet_id, range):
+def get_google_sheet(spreadsheet_id, range_name):
     credentials = authorize_credentials()
     http = credentials.authorize(httplib2.Http())
     discovery_url = 'https://sheets.googleapis.com/$discovery/rest?version=v4'
     service = build('sheets', 'v4', http=http, discoveryServiceUrl=discovery_url)
-    range_name = range
     result = service.spreadsheets().values().get(
         spreadsheetId=spreadsheet_id, range=range_name).execute()
     values = result.get('values', [])
@@ -41,37 +42,41 @@ def main():
     values = get_google_sheet('1Jh3ehawbeYeLSsCBQFuO_MKqE6IjBDBByY6qBvC7K-k', "Responses Copy")
     strats = get_google_sheet('1Jh3ehawbeYeLSsCBQFuO_MKqE6IjBDBByY6qBvC7K-k', "Lookup!A13:C28")
 
+    class_num = "200"
+
     colors = []
     strat_strings = []
 
     for i in range(len(strats)):
-        strat_strings.append(strats[i][0])
         if strats[i][2] == "P":
             colors.append("red")
         elif strats[i][2] == "A":
             colors.append("blue")
 
-    avgs = [0] * 13
-    sum_strats = [0] * 17
-    sums = [0] * 17
-    class_num = "101"
+    avgs = [0] * len(colors)
+    tmp_sum_strats = [0] * (len(strats))
+    tmp_sums = [0] * (len(strats))
 
     for i in range(len(values)):
         if values[i][2] == class_num:
             tmp = values[i][5].split(",")
             for j in range(len(tmp)):
                 if tmp[j] != "":
-                    sum_strats[int(tmp[j])] = sum_strats[int(tmp[j])] + 1
-                    sums[(int(tmp[j]))] = sums[(int(tmp[j]))] + int(values[i][10])
+                    tmp_sum_strats[int(tmp[j])] = tmp_sum_strats[int(tmp[j])] + 1
+                    tmp_sums[(int(tmp[j]))] = tmp_sums[(int(tmp[j]))] + int(values[i][10])
 
-    sum_strats.pop(0)
-    sums.pop(0)
-    sum_strats.pop(3)
-    sums.pop(3)
-    sum_strats.pop(10)
-    sums.pop(10)
-    sum_strats.pop(13)
-    sums.pop(13)
+    print(tmp_sum_strats)
+    print(tmp_sums)
+
+    sum_strats = []
+    sums = []
+
+    for i in range(len(tmp_sum_strats)):
+        if strats[i][2] != "N/A":
+            sum_strats.append(tmp_sum_strats[i])
+            sums.append(tmp_sums[i])
+            strat_strings.append(strats[i][0])
+
     print(sum_strats)
     print(sums)
 
@@ -81,27 +86,27 @@ def main():
         else:
             avgs[i] = int(sums[i] / sum_strats[i])
 
-    print(avgs)
-
-    avgs, strat_strings = (list(t) for t in zip(*sorted(zip(avgs, strat_strings))))
+    avgs, strat_strings, colors = (list(t) for t in zip(*sorted(zip(avgs, strat_strings, colors))))
     print(avgs)
 
     for i in range(len(avgs)):
         if avgs[0] == 0:
             avgs.pop(0)
             strat_strings.pop(0)
+            colors.pop(0)
+
+    strat_strings = [textwrap.fill(text, 15) for text in strat_strings]
 
     index = np.arange(len(strat_strings))
-    plt.bar(index, avgs, align='edge', color=['blue', 'red', 'red', 'blue', 'red', 'red'])
+    plt.bar(index, avgs, align='center', color=colors)
     plt.xlabel('Study Strategies', fontsize=15)
     plt.ylabel('Averages', fontsize=15)
-    plt.xticks(index, strat_strings, fontsize=8, rotation=30)
+    plt.xticks(index, strat_strings, fontsize=10)
     plt.title('BIO ' + class_num + ' Exam #1', fontsize=20)
-    plt.subplots_adjust(left=0.05, right=0.95, bottom=0.25, top=0.95)
+    plt.subplots_adjust(left=0.05, right=0.95, bottom=0.20, top=0.95)
     figure = plt.gcf()
-    figure.set_size_inches(20, 9)
+    figure.set_size_inches(20, 8)
     figure.savefig('BIO ' + class_num + '.png', dpi=150)
 
 
-if __name__ == main():
-    main()
+main()
